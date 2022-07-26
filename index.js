507b62a8124bb14e72fc2042f0825c0e1e2c377d
minki jung <qmsoqm2@gmail.com>
2022-07-26 10:34:01 -0400

    s

--- index.js
+++ index.js
@@ -27,49 +27,49 @@ app.get('/', (req, res) => {
 app.get('/%EC%96%B4%EB%A6%B0%EC%9D%B4', (req, res) => {
     res.send(books2)
 })
-app.get('/청소년', (req, res) => {
+app.get('/%EC%B2%AD%EC%86%8C%EB%85%84', (req, res) => {
     res.send(books3)
 })
-app.get('/좋은부모', (req, res) => {
+app.get('/%EC%A2%8B%EC%9D%80%EB%B6%80%EB%AA%A8', (req, res) => {
     res.send(books4)
 })
-app.get('/소설+시+희곡', (req, res) => {
+app.get('/%EC%86%8C%EC%84%A4+%EC%8B%9C+%ED%9D%AC%EA%B3%A1', (req, res) => {
     res.send(books5)
 })
-app.get('/에세이', (req, res) => {
+app.get('/%EC%97%90%EC%84%B8%EC%9D%B4', (req, res) => {
     res.send(books6)
 })
-app.get('/인문학', (req, res) => {
+app.get('/%EC%9D%B8%EB%AC%B8%ED%95%99', (req, res) => {
     res.send(books7)
 })
-app.get('/사회과학', (req, res) => {
+app.get('/%EC%82%AC%ED%9A%8C%EA%B3%BC%ED%95%99', (req, res) => {
     res.send(books8)
 })
-app.get('/역사', (req, res) => {
+app.get('/%EC%97%AD%EC%82%AC', (req, res) => {
     res.send(books9)
 })
-app.get('/과학', (req, res) => {
+app.get('/%EA%B3%BC%ED%95%99', (req, res) => {
     res.send(books10)
 })
-app.get('/예술+대중문화', (req, res) => {
+app.get('/%EC%98%88%EC%88%A0+%EB%8C%80%EC%A4%91%EB%AC%B8%ED%99%94', (req, res) => {
     res.send(books11)
 })
-app.get('/종교+역학', (req, res) => {
+app.get('/%EC%A2%85%EA%B5%90+%EC%97%AD%ED%95%99', (req, res) => {
     res.send(books12)
 })
-app.get('/경제경영', (req, res) => {
+app.get('/%EA%B2%BD%EC%A0%9C%EA%B2%BD%EC%98%81', (req, res) => {
     res.send(books13)
 })
-app.get('/자기계발', (req, res) => {
+app.get('/%EC%9E%90%EA%B8%B0%EA%B3%84%EB%B0%9C', (req, res) => {
     res.send(books14)
 })
-app.get('/외국어', (req, res) => {
+app.get('/%EC%99%B8%EA%B5%AD%EC%96%B4', (req, res) => {
     res.send(books15)
 })
-app.get('/가정+요리+뷰티', (req, res) => {
+app.get('/%EA%B0%80%EC%A0%95+%EC%9A%94%EB%A6%AC+%EB%B7%B0%ED%8B%B0', (req, res) => {
     res.send(books16)
 })
-app.get('/건강+취미+레저', (req, res) => {
+app.get('/%EA%B1%B4%EA%B0%95+%EC%B7%A8%EB%AF%B8+%EB%A0%88%EC%A0%80', (req, res) => {
     res.send(books17)
 })
 app.get('/기타', (req, res) => {
