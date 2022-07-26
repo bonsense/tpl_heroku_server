cf623792a9241e90c7e38bd9187f293a7a4f4765
minki jung <qmsoqm2@gmail.com>
2022-07-26 11:39:33 -0400

    s

--- index.js
+++ index.js
@@ -6,25 +6,6 @@ app.use(cors())
 
 const port = process.env.PORT || 3000
 
-// "all" = books
-// "어린이" = children
-// "청소년" = youngadult
-// "좋은부모" = parenting
-// "소설/시/희곡" = novelpoetryplay
-// "에세이" = essay
-// "인문학" = humanities
-// "사회과학" = sociology
-// "역사" = history
-// "과학" = science
-// "예술/대중문화" = artculture
-// "종교/역학" = religion
-// "경제경영" = economy
-// "자기계발" = selfhelp
-// "외국어" = foreignlanguage
-// "가정/요리/뷰티" = homecookbeauty
-// "건강/취미/레저" = healthleisure
-// "기타" = etc
-
 const books = require('./books.json')["books"]
 const children = books.filter(d => d.category == '어린이');
 const youngadult = books.filter(d => d.category == '청소년');
@@ -59,7 +40,7 @@ app.get('/parenting', (req, res) => {
 app.get('/novelpoetryplay', (req, res) => {
     res.send(novelpoetryplay)
 })
-app.get('/essay, (req, res) => {
+app.get('/essay', (req, res) => {
     res.send(essay)
 })
 app.get('/humanities', (req, res) => {
